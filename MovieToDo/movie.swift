e8411909e717a3a0d42cc146b15dc1528101efd6
Rushabh Shah <rushabh795@gmail.com>
2022-08-01 13:53:27 -0400

    -Added TableViewConroller -create layout -add model class and array

---
+++ MovieToDo/movie.swift
@@ -0,0 +1,15 @@
+//
+//  movie.swift
+//  MovieToDo
+//
+//  Created by Rushabh Shah on 2022-07-31.
+//
+
+//model class
+import Foundation
+class Movie {
+    var title: String
+    init(title: String) {
+        self.title = title
+    }
+}
